75b2dd0801e007d7f7103a37ef8f18971d263538
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-06-03 03:28:02 +0800

    add search functionality with material search bar

--- app/src/main/java/com/rahmat/app/cataloguemovie/MainActivity.java
+++ app/src/main/java/com/rahmat/app/cataloguemovie/MainActivity.java
@@ -3,8 +3,11 @@ package com.rahmat.app.cataloguemovie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.rahmat.app.cataloguemovie.model.Movie;
 import com.rahmat.app.cataloguemovie.model.MovieResult;
 import com.rahmat.app.cataloguemovie.rest.MovieClient;
@@ -13,22 +16,40 @@ import com.rahmat.app.cataloguemovie.rest.MovieInterface;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        MaterialSearchBar.OnSearchActionListener{
 
     private final static String API_KEY = BuildConfig.ApiKey;
     List<MovieResult> movieList;
+
+    //@BindView(R.id.searchBar)
+    MaterialSearchBar materialSearchBar;
+
+    MovieInterface movieService;
+    Call<Movie> movieCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MovieInterface movieService = MovieClient.getClient().create(MovieInterface.class);
-        Call<Movie> movieCall = movieService.getMovieBySearch("captain", API_KEY);
+        //ButterKnife.bind(this);
+        materialSearchBar = findViewById(R.id.searchBar);
+
+        materialSearchBar.setOnSearchActionListener(this);
+        materialSearchBar.inflateMenu(R.menu.main);
+
+    }
+
+    private void getMovies(String query){
+        movieService = MovieClient.getClient().create(MovieInterface.class);
+        movieCall = movieService.getMovieBySearch(query, API_KEY);
 
         movieList = new ArrayList<>();
 
@@ -45,5 +66,42 @@ public class MainActivity extends AppCompatActivity {
                         , Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void getMovies(){
+        movieService = MovieClient.getClient().create(MovieInterface.class);
+        movieCall = movieService.getPopularMovie(API_KEY);
+
+        movieList = new ArrayList<>();
+
+        movieCall.enqueue(new Callback<Movie>() {
+            @Override
+            public void onResponse(Call<Movie> call, Response<Movie> response) {
+                movieList = response.body().getResults();
+                Log.v("Matt", "Number of movie with  = "+response.body().getTotalResults());
+            }
+
+            @Override
+            public void onFailure(Call<Movie> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Something went wrong"
+                        , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+        String q = String.valueOf(text);
+        getMovies(q);
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
     }
 }
