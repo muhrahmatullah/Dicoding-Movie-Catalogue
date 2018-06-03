a31e6b46ebf8a73098dad7b0a7be865814dd6f85
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-06-03 17:37:08 +0800

    add initial code for detail activity

---
+++ app/src/main/java/com/rahmat/app/cataloguemovie/MovieDetailActivity.java
@@ -0,0 +1,13 @@
+package com.rahmat.app.cataloguemovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
