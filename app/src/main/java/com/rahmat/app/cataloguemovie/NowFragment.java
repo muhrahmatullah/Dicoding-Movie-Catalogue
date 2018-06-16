bd84d74e994cefe485272129cd85d4ece419b648
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-06-16 16:07:29 +0800

    added fragments

---
+++ app/src/main/java/com/rahmat/app/cataloguemovie/NowFragment.java
@@ -0,0 +1,43 @@
+package com.rahmat.app.cataloguemovie;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NowFragment extends Fragment {
+
+
+    public NowFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_now, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
